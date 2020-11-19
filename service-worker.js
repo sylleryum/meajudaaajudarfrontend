4fdc4d6ad120e49daa3742600579134906c0ea70
sylleryum <alisson_piuco@yahoo.com.br>
2020-11-19 11:53:38 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meajudaaajudarfrontend/precache-manifest.87d00c290a117d612322a080a16f3187.js"
+  "/meajudaaajudarfrontend/precache-manifest.0b97b4502081f864677a4f857517f7ac.js"
 );
 
 self.addEventListener('message', (event) => {
